3414c1e033992f8dc1d1ab1af5b0051b5a2cbb18
MiguelCoder <migueltecnicoinfo@gmail.com.com>
2026-07-28 01:35:31 -0300

    Minha primeira aula de C#

--- Program.cs
+++ Program.cs
@@ -3,13 +3,13 @@
 string nome = "Miguel";
 string profissao = "técnico em informática";
 int idade = 31;
-double altura= 1.57;
+double altura = 1.57;
 char letra = 'A';
 bool estudando = true;
 Console.WriteLine("Aqui estão os dados das variáveis:");
 Console.WriteLine("String: " +nome);
 Console.WriteLine("Int: "+ idade);
-Console.WriteLine("Double: "+letra);
+Console.WriteLine("Double: "+altura);
 Console.WriteLine("Bool: "+ estudando);
 
 //Interpolação:
