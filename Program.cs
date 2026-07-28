3691c1a4bf56e9ba1eae8454166f920fb50ef592
MiguelCoder <migueltecnicoinfo@gmail.com.com>
2026-07-28 01:08:07 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Variáveis:");
+
+string nome = "Miguel";
+int idade = 31;
+double altura= 1.57;
+char letra = 'A';
+bool estudando = true;
+Console.WriteLine("Aqui estão os dados das variáveis:");
+Console.WriteLine("String: " +nome);
+Console.WriteLine("Int: "+ idade);
+Console.WriteLine("Double: "+letra);
+Console.WriteLine("Bool: "+ estudando);
